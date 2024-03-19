162c8254c9b08fc887749e9cc397f9366d6c772f
Lucas Gladding <l.gladding@vehikl.com>
2024-03-19 17:52:47 -0400

    Updated structure.

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,16 +1,17 @@
 export default function Page() {
     return (
-        <div>
-            <h1>Contacts</h1>
-            <div>
-                <div className="flex gap-2">
+        <>
+            <header>Contacts</header>
+            <main>
+                <article className="flex gap-2">
                     <div>John</div>
                     <div>Smith</div>
                     <div>123 Main Street</div>
                     <div>Waterloo</div>
+                    <div>john.smith@example.com</div>
                     <div>519-555-1234</div>
-                </div>
-            </div>
-        </div>
+                </article>
+            </main>
+        </>
     )
 }
