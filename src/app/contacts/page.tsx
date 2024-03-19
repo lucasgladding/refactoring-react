3d57ecb7640b83b827fa5600864e822cf0378741
Lucas Gladding <l.gladding@vehikl.com>
2024-03-19 17:50:11 -0400

    Added contacts page.

---
+++ src/app/contacts/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Contacts</h1>
+            <div>
+                <div className="flex gap-2">
+                    <div>John</div>
+                    <div>Smith</div>
+                    <div>123 Main Street</div>
+                    <div>Waterloo</div>
+                    <div>519-555-1234</div>
+                </div>
+            </div>
+        </div>
+    )
+}
