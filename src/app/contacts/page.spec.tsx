4a75d601c883c0d683a75f48a1e8b47b55697bf4
Lucas Gladding <l.gladding@vehikl.com>
2024-03-20 07:54:49 -0400

    Added test for static contacts.

--- src/app/contacts/page.spec.tsx
+++ src/app/contacts/page.spec.tsx
@@ -1,10 +1,20 @@
-import { describe, expect, it } from 'vitest'
-import { render, screen } from '@testing-library/react';
+import { afterEach, describe, expect, it } from 'vitest'
+import { cleanup, render, screen } from '@testing-library/react';
 import Contacts from './page';
+
+afterEach(() => {
+    cleanup()
+})
 
 describe('contacts', () => {
     it('renders the page', () => {
         render(<Contacts />)
         expect(screen.getByText('Contacts')).toBeDefined()
+    })
+
+    it('renders some contacts', () => {
+        render(<Contacts />)
+        expect(screen.getByText('John Smith')).toBeDefined()
+        expect(screen.getByText('Jane Smith')).toBeDefined()
     })
 })
